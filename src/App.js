21607cbb03fc86b7a59b937440c590f6335f4d98
Nevin10son <nevintenson235@gmail.com>
2024-06-10 12:52:28 +0530

    friend app completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddFriend from './component/AddFriend';
+import ViewAll from './component/ViewAll';
 
 function App() {
   return (
     <div className="App">
      <AddFriend/>
+     <ViewAll/>
     </div>
   );
 }
--- src/component/AddFriend.jsx
+++ src/component/AddFriend.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const AddFriend = () => {
+    const [data,setdata]=useState(
+        {
+            "name": "",
+            "friendName": "",
+            "friendNickName":"",
+            "DescribeYourFriend":""
+        }
+    )
+    const inputHandler = (event)=>{
+        setdata({...data,[event.target.name]:event.target.value})
+    }
+    const readValues=()=>{
+        console.log(data)
+        axios.post("https://friendsapi-re5a.onrender.com/adddata",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Added Successfully")
+                    
+                } else {
+                    alert("Somethings Wrong")
+                    
+                }
+            }
+        )
+    }
+
   return (
     <div>
       <div className="container">
@@ -9,22 +37,22 @@ const AddFriend = () => {
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="" className="form-label">Name:</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="" className="form-label">Friend Name:</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='friendName' value={data.friendName} onChange={inputHandler}/>
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">Friend Name:</label>
-                        <input type="text" className="form-control" />
+                        <label htmlFor="" className="form-label">Friend Nick Name:</label>
+                        <input type="text" className="form-control" name='friendNickName' value={data.friendNickName} onChange={inputHandler}/>
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="" className="form-label">Friend Desc:</label>
-                        <textarea name="" id="" className="form-control"></textarea>
+                        <textarea name="DescribeYourFriend" value={data.DescribeYourFriend} onChange={inputHandler} className="form-control"></textarea>
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-success">Add</button>
+                        <button className="btn btn-success" onClick={readValues}>Add</button>
                     </div>
                 </div>
             </div>
